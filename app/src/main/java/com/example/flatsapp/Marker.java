0df8a2f2fc5f74248f1a705ee3773525c57a1d50
Trinh Dung <c170017@e.ntu.edu.sg>
2019-03-04 14:56:03 +0800

    uploading test ClusterItem

---
+++ app/src/main/java/com/example/flatsapp/Marker.java
@@ -0,0 +1,56 @@
+package com.example.flatsapp;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class Marker implements ClusterItem {
+    private LatLng position;
+    private String title;
+    private String snippet;
+    private int iconPic;
+
+    public Marker(LatLng position, String title, String snippet, int iconPic) {
+        this.position = position;
+        this.title = title;
+        this.snippet = snippet;
+        this.iconPic = iconPic;
+    }
+
+    public Marker() {
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return position;
+    }
+
+    public void setPosition(LatLng position) {
+        this.position = position;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String getSnippet() {
+        return snippet;
+    }
+
+    public void setSnippet(String snippet) {
+        this.snippet = snippet;
+    }
+
+    public int getIconPic() {
+        return iconPic;
+    }
+
+    public void setIconPic(int iconPic) {
+        this.iconPic = iconPic;
+    }
+}
